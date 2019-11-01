bd9febd76097e558e25ab1dea0fd6a17f66263be
Softwarica College Lab <softwarica@softwarica.com>
2019-11-01 12:51:50 +0545

    Complete Login Design

--- app/src/main/java/www/myandroidcode/mydoctor/Registration.java
+++ app/src/main/java/www/myandroidcode/mydoctor/Registration.java
@@ -1,13 +1,49 @@
 package www.myandroidcode.mydoctor;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class Registration extends AppCompatActivity {
+import java.util.Calendar;
+
+public class Registration extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    private EditText txtBirthDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        txtBirthDate=findViewById(R.id.txtBirthDate);
+
+        txtBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePicker();
+            }
+        });
+
+
+
+    }
+
+    public void DatePicker(){
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = 1+calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,this,year,month,day);
+        datePickerDialog.show();
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        txtBirthDate.setText(year+"/"+month+"/"+dayOfMonth);
     }
 }
